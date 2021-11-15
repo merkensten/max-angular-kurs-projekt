27a0df3ca67f9d3ecde8e2f65b187e086efcc7de
Mattis Erkensten <m.erkensten@icloud.com>
2021-11-15 11:06:01 +0100

    Routing sektionen klar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +7,6 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    console.log('selected feature: ', feature);
-    this.featureSelected.emit(feature);
-  }
 
   constructor() {}
 
